832f03cab4e3ae8be50f6aa963be577e79661b81
P4t4m8n <eranopenuni@gmail.com>
2024-02-23 12:56:40 +0200

    Update auth route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,21 @@ import { ContactDetailsComponent } from './pages/contact-details/contact-details
 import { StatisticsComponent } from './pages/statistics/statistics.component';
 import { ContactEditComponent } from './pages/contact-edit/contact-edit.component';
 import { contactResolver } from './resolvers/contact.resolver';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'statistics', component: StatisticsComponent },
   {
     path: 'contacts', component: ContactIndexComponent, children: [
-      { path: ':contactId', component: ContactDetailsComponent },
-      { path: 'edit/:contactId', component: ContactEditComponent, resolve: { contact: contactResolver } },
+      {
+        path: ':contactId', component: ContactDetailsComponent,
+        canActivate: [authGuard], resolve: { contact: contactResolver }
+      },
+      {
+        path: 'edit/:contactId', component: ContactEditComponent,
+        resolve: { contact: contactResolver }
+      },
       { path: 'edit/', component: ContactEditComponent },
 
     ]
