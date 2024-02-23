3027e7f2f27efa1b8c788a5ab3544b9239d69e32
P4t4m8n <eranopenuni@gmail.com>
2024-02-23 12:53:43 +0200

    Update user service add gourds and resolvers

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,23 +1,23 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { User } from "../models/user.model";
+import { Injectable } from "@angular/core"
+import { BehaviorSubject } from "rxjs"
+import { User } from "../models/user.model"
 
 const ENTITY = 'user'
-
-
-
 @Injectable({
     providedIn: 'root'
 })
-export class userService {
+export class UserService {
 
     private _user$ = new BehaviorSubject<User>({
         name: "Puki Ben David",
         coins: 100,
         moves: [],
-        imgUrl:''
+        imgUrl: ''
     })
     public user$ = this._user$.asObservable()
 
-   
+    getLoggedInUser() {
+        return this._user$.value
+    }
+
 }
